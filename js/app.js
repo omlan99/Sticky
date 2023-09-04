0ff5e955ba89bc9b6507433a6b658005dd24157d
omlan99 <95955202+omlan99@users.noreply.github.com>
2023-09-04 23:53:46 +0600

    code

---
+++ js/app.js
@@ -0,0 +1,28 @@
+
+            //  window.addEventListener("scroll",function(){
+            //     document.getElementById('navbar').classList.add("sticky");
+            //   })           
+
+            // window.addEventListener('scroll',function(){
+            //   let abc = `scrollToTop:window`;
+            //   if(abc>100){
+            //     alert("your have scrolled 100");
+            //   }
+            //   console.log(abc);
+            // })
+            $(function () {
+              let _window = $(window);
+              let nav = $("#nav");
+              _window.on("scroll", function (){
+                  let scrollToTop = _window.scrollTop();
+          
+                  if (scrollToTop > 80) { 
+                      nav.addClass("sticky");
+                  } else {
+                      nav.removeClass("sticky");
+                  }
+              });
+              _window.on("scroll", function(){
+                $(".preloader").hide();
+              })
+          });
